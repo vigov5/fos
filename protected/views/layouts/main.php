46ec68095720fdf52ea725dcec8242af57fd8e2d
tunghdt91 <tunghdt91@gmail.com>
2013-07-18 14:01:45 +0700

    task#5196 create navigation bar view

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,86 +1,103 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="language" content="en" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="en" />
+        <?php
+        Yii::app()->bootstrap->register();
+        Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl . '/css/style.css');
+        Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl . '/css/main.css');
+        Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl . '/css/form.css');
+        ?>
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
+    <body>
+        <div class="container" id="page">
+            <div id="header">
+                <div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+            </div><!-- header -->
 
-    <!-- blueprint CSS framework -->
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-    <!--[if lt IE 8]>
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-    <![endif]-->
-      <?php Yii::app()->bootstrap->register(); ?>      
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
-    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>
+            <div id="mainmenu">
+                <?php
+                if (Yii::app()->user->isGuest) {
+                    $this->widget('bootstrap.widgets.TbNavbar', array(
+                        'type' => 'inverse', // null or 'inverse'
+                        'brand' => 'framgia',
+                        'brandUrl' => '#',
+                        'collapse' => true, // requires bootstrap-responsive.css
+                        'items' => array(
+                            array(
+                                'class' => 'bootstrap.widgets.TbMenu',
+                                'items' => array(
+                                    array('label' => 'Home', 'icon' => 'home white', 'url' => array('home/index')),
+                                    array('label' => '', 'icon' => 'star', 'url' => '#'),
+                                    array('label' => '', 'icon' => 'envelope', 'url' => '#'),
+                                    array('label' => '', 'icon' => 'globe', 'url' => '#'),
+                                ),
+                            ),
+                            array(
+                                'class' => 'bootstrap.widgets.TbMenu',
+                                'htmlOptions' => array('class' => 'pull-right'),
+                                'items' => array(
+                                    array('label' => 'SignIn', 'icon' => 'user white',
+                                        'url' => array('user/signin')
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ));
+                } else {
+                    $this->widget('bootstrap.widgets.TbNavbar', array(
+                        'type' => 'inverse', // null or 'inverse'
+                        'brand' => 'framgia',
+                        'brandUrl' => '#',
+                        'collapse' => true, // requires bootstrap-responsive.css
+                        'items' => array(
+                            array(
+                                'class' => 'bootstrap.widgets.TbMenu',
+                                'items' => array(
+                                    array('label' => 'Home', 'icon' => 'home white', 'url' => array('home/index')),
+                                    array('label' => '', 'icon' => 'star', 'url' => '#'),
+                                    array('label' => '', 'icon' => 'envelope', 'url' => '#'),
+                                    array('label' => '', 'icon' => 'globe', 'url' => '#'),
+                                ),
+                            ),
+                            '<form class="navbar-search pull-left" action=""><input type="text"
+                             class="search-query span4" placeholder="search people, post and anythings">
+                            </form>',
+                            array(
+                                'class' => 'bootstrap.widgets.TbMenu',
+                                'htmlOptions' => array('class' => 'pull-right'),
+                                'items' => array(
+                                    array('label' => Yii::app()->user->username, 'icon' => 'user white', 'url' => '#',
+                                        'items' => array(
+                                            array('label' => 'Profile', 'icon' => 'pencil', 'url' => '#'),
+                                            array('label' => 'Settings', 'icon' => 'cog', 'url' => '#'),
+                                            array('label' => 'Help', 'icon' => 'flag', 'url' => '#'),
+                                            '---',
+                                            array('label' => 'Logout', 'icon' => 'icon-share', 'url' => array('user/signout')),
+                                        )),
+                                ),
+                            ),
+                        ),
+                    ));
+                }
+                ?>
+            </div><!-- mainmenu -->
 
-<body>
+            <div class="clear"></div>
+            <div class="content">
+                <?php echo $content; ?>
+            </div>
+            <div class="clear"></div>
 
-<div class="container" id="page">
+            <div id="footer">
+                Copyright &copy; <?php echo date('Y'); ?> by Framgia.<br/>
+                All Rights Reserved.<br/>                        
+                <?php echo Yii::powered(); ?>                   
+            </div><!-- footer -->
+        </div><!-- page -->
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
-
-	<div id="mainmenu">
-    <?php $this->widget('bootstrap.widgets.TbNavbar', array(
-        'type'=>'inverse', // null or 'inverse'
-        'brand'=>'Framgia Fos',
-        'brandUrl'=>'#',
-        'collapse'=>true, // requires bootstrap-responsive.css
-        'items'=>array(
-         array(
-            'class'=>'bootstrap.widgets.TbMenu',
-            'items'=>array(
-                array('label'=>'Home', 'url'=>'#', 'active'=>true),
-                array('label'=>'Link', 'url'=>'#'),
-                array('label'=>'Dropdown', 'url'=>'#', 'items'=>array(
-                    array('label'=>'Action', 'url'=>'#'),
-                    array('label'=>'Another action', 'url'=>'#'),
-                    array('label'=>'Something else here', 'url'=>'#'),
-                    '---',
-                    array('label'=>'NAV HEADER'),
-                    array('label'=>'Separated link', 'url'=>'#'),
-                    array('label'=>'One more separated link', 'url'=>'#'),
-                      )),
-                    ),
-            ),
-        '<form class="navbar-search pull-left" action=""><input type="text" class="search-query span2" placeholder="Search"></form>',
-        array(
-            'class'=>'bootstrap.widgets.TbMenu',
-            'htmlOptions'=>array('class'=>'pull-right'),
-            'items'=>array(
-                array('label'=>'Link', 'url'=>'#'),
-                array('label'=>'Dropdown', 'url'=>'#', 'items'=>array(
-                    array('label'=>'Action', 'url'=>'#'),
-                    array('label'=>'Another action', 'url'=>'#'),
-                    array('label'=>'Something else here', 'url'=>'#'),
-                    '---',
-                    array('label'=>'Separated link', 'url'=>'#'),
-                    )),
-                ),
-            ),
-            ),
-        )); 
-    ?>
-	</div><!-- mainmenu -->
-    
-    <div id="content">
-        <?php echo $content; ?>
-    </div>
-	<div class="clear"></div>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
-</body>
+    </body>
 </html>
