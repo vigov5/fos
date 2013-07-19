bd2fba1aa07bbec41c0629b5827a20b1ef8a2bd6
Nguyen Trung Quan <trungquana69h@gmail.com>
2013-07-19 16:45:19 +0700

    Task#5312 Add link to change pass page and fix change pass page layout

--- protected/views/user/change_password.php
+++ protected/views/user/change_password.php
@@ -1,35 +1,35 @@
-<div style="height:50px"></div>
-<div>
-    <div>
+<?php $this->widget('bootstrap.widgets.TbAlert'); ?>
+<div class="center_form">
+    <div class="row text-center">
         <h1>Change Password</h1>
     </div>
-</div>
-<?php $this->widget('bootstrap.widgets.TbAlert'); ?>
-<div>
     <form method="Post">      
         <div>
             <div style="height:50px"></div>
             <?php echo CHtml::errorSummary($form); ?>      
-            <div>
-                <div>                
-                    <?php echo CHtml::activePasswordField($form, 'oldPass', array('class' => '', 
-                        'placeholder' => 'Old Password')); ?>
-                </div>
+            <div class="row">            
+                <?php echo CHtml::activePasswordField($form, 'oldPass', array(
+                        'class' => 'span6 offset3', 
+                        'placeholder' => 'Old Password',
+                    ));
+                ?>
             </div>
-            <div>
-                <div>                
-                    <?php echo CHtml::activePasswordField($form, 'newPass', array('class' => '', 
-                        'placeholder' => 'New Password')); ?>
-                </div>
+            <div class="row">           
+                <?php echo CHtml::activePasswordField($form, 'newPass', array(
+                        'class' => 'span6 offset3', 
+                        'placeholder' => 'New Password',
+                    ));
+                ?>
             </div>
-            <div>
-                <div>            
-                    <?php echo CHtml::activePasswordField($form, 'passConfirm', array('class' => '', 
-                        'placeholder' => 'New Password Confirmation')); ?>
-                </div>
+            <div class="row">        
+                <?php echo CHtml::activePasswordField($form, 'passConfirm', array(
+                    'class' => 'span6 offset3', 
+                    'placeholder' => 'New Password Confirmation',
+                    ));
+                ?>
             </div>
-            <div style="height:50px"></div>            
-            <div><?php echo CHtml::submitButton('Submit'); ?></div>                            
+            <div class="none"></div>            
+            <div class="row"><?php echo CHtml::submitButton('Submit', array('class' => 'btn btn-primary span2 offset5')); ?></div>                            
         </div>
     </form>
 </div>
