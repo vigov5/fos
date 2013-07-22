50ed061416652ab6322ae8d51533a837475abb01
Tran Duc Thang <thangtd90@macbook-023.local>
2013-07-22 16:56:11 +0700

    Bug #5363 Layout CSS Error

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -104,13 +104,8 @@
                         ?>
                     </div>
                 <?php } ?>
-            </div>            
-            <div class="clear"></div>
+            </div>                        
         </div><!-- page -->        
     </body>
-    <div id="footer">
-        Copyright &copy; <?php echo date('Y'); ?> by Framgia.<br/>
-        All Rights Reserved.<br/>
-        <?php echo Yii::powered(); ?>
-    </div><!-- footer -->
+    
 </html>
