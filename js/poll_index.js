e5820a2066b8d28374c51d243f466b531451d5a9
Van Giang <giangnv91@gmail.com>
2013-07-29 11:02:21 +0700

    Task #5605 fix search filter css/layout

--- js/poll_index.js
+++ js/poll_index.js
@@ -2,8 +2,10 @@ $(function() {
     $('#show_search').click(function() {
         if ($('#form_search').is(':hidden')) {
             $('#form_search').show(HIDE_TIME);
+            $(this).val('Hide Search');
         } else {
             $('#form_search').hide(HIDE_TIME);
+            $(this).val('Show Search');
         }
     });
     $('#multichoice').prop('disabled', true);
