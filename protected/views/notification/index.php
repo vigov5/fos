0ac5e46f58ecafe9c111919679a438209a77a64a
Nguyen Trung Quan <trungquana69h@gmail.com>
2013-08-01 17:25:59 +0700

    Task#5869 Load more notifications at all notifications page

--- protected/views/notification/index.php
+++ protected/views/notification/index.php
@@ -3,3 +3,8 @@
 ?>
 <h1>Your Notifications</h1>
 <div id="notification-list"></div>
+<div class="end_activity">
+<?php
+echo CHtml::button('Load more', array('class' => 'btn btn-primary', 'id' => 'load_more_btn'));
+?>
+</div>
