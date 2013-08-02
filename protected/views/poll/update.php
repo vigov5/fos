72027df77993810cb5eddcfdfaa2bbdcbc619a89
Nguyen Anh Tien <nguyenanhtien2210@gmail.com>
2013-08-02 10:53:50 +0700

    Bug#5901 fix some minor bug and change port

--- protected/views/poll/update.php
+++ protected/views/poll/update.php
@@ -1,15 +1,3 @@
-<div class='clear'></div>
-<?php
-$this->widget('zii.widgets.CBreadcrumbs', array(
-    'links' => array(
-        'Home' => array('home/index'),
-    ),
-    'links' => array(
-        'UpdatePoll' => array('poll/index'),
-        "#{$poll->id}" =>array('poll/view','id' => $poll->id)
-    ),
-));
-?>
 <div class='row'>
     <div class='span6'><h3>Update Poll</h3></div>
 </div>
